502cb0dc516e48862563cee46d1f0b645681bec2
chan <nch6807@naver.com>
2025-06-15 19:49:56 +0900

    추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/next";
 import Header from "../components/header";
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Header />
       <Component {...pageProps} />
       <Analytics /> {/* ✅ 여기 */}
+      <SpeedInsights /> {/* 👈 여기에 추가 */}
     </>
   );
 }
