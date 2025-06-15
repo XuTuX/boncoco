f9a95b4c8b77560670e98566fb3c15e1bc7b83c5
chan <nch6807@naver.com>
2025-06-15 10:25:50 +0900

    병리학

--- data/questions.ts
+++ data/questions.ts
@@ -1,10 +1,15 @@
 // data/questions.ts
 import { bonchoQuiz } from "./bonchoQuiz";
 import { jindanQuiz } from "./jindanQuiz";
+import { modernPhysiology } from "./modernPhysiology";
+import { hanbyungrihak } from "./hanbyungrihak";
 
 export const quizByCategory: {
     [major: string]: { [sub: string]: { question: string; answer: string }[] };
 } = {
     본초학: bonchoQuiz,
     진단학: jindanQuiz,
+    양방생리: modernPhysiology,
+    병리학: hanbyungrihak,
 };
+
