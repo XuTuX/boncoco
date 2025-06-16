e2d78ffc0a7baed6ac734da29b7459f936f5d586
chan <nch6807@naver.com>
2025-06-17 02:35:53 +0900

    양생 기출

--- data/questions.ts
+++ data/questions.ts
@@ -5,6 +5,8 @@ import { modernPhysiology } from "./modernPhysiology";
 import { hanbyungrihak } from "./hanbyungrihak";
 import { sanghan } from "./sanghan";
 import { sanghanQuiz } from "./sanghanQuiz";
+import { moderntest } from "./moderntest";
+
 
 export const quizByCategory: {
     [major: string]: { [sub: string]: { question: string; answer: string }[] };
@@ -14,6 +16,8 @@ export const quizByCategory: {
     양방생리: modernPhysiology,
     병리학: hanbyungrihak,
     상한: sanghan,
-    상한_객관식_주관식: sanghanQuiz
+    상한_객관식_주관식: sanghanQuiz,
+    양방생리_기출만: moderntest
+
 };
 
